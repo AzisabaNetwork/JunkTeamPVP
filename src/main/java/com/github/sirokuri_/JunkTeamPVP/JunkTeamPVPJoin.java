8f5b6389f58285d2ed3eb562d3183350844111cb
46kuri_ <ghost.46kuri@gmail.com>
2021-12-23 22:54:30 +0900

    Listに入っているか確認できるデバッグ用機能のテスト

--- src/main/java/com/github/sirokuri_/JunkTeamPVP/JunkTeamPVPJoin.java
+++ src/main/java/com/github/sirokuri_/JunkTeamPVP/JunkTeamPVPJoin.java
@@ -7,7 +7,9 @@ import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class JunkTeamPVPJoin implements Listener {
     private final JunkTeamPVP plugin;
@@ -21,12 +23,14 @@ public class JunkTeamPVPJoin implements Listener {
         Player player = e.getPlayer();
         Block block = e.getClickedBlock();
         if(block == null) return;
+        if ((e.getHand() != EquipmentSlot.HAND || e.getAction() == Action.LEFT_CLICK_BLOCK)) return;
         if(block.getType() == Material.OAK_WALL_SIGN){
             Sign sign = (Sign) block.getState();
             String firstSignLine = sign.getLine(0);
             String secondSignLine = sign.getLine(1);
             if(firstSignLine.equals("[JunkTeamPVP]") && secondSignLine.equals("試合に参加する")){
                 if (!plugin.onlinePlayers.contains(player)) {
+                    player.sendMessage(ChatColor.GREEN + "ゲームスタートロビーに移動しました");
                     plugin.onlinePlayers.add(player);
                     if (plugin.onlinePlayers.size() % 2 == 0) {
                         plugin.redTeam.add(player);
@@ -43,6 +47,8 @@ public class JunkTeamPVPJoin implements Listener {
                     return;
                 }
             }
-        }
+        }/*else{
+            player.sendMessage("" + plugin.blueTeam.contains(player) + "\n" + plugin.redTeam.contains(player) + "\n" + plugin.onlinePlayers.contains(player));
+        }*/
     }
 }
