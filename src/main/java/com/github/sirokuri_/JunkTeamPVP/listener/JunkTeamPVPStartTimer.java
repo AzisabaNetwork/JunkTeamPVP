0017644c4d039bfc1c8deb7ccf8c9ee03f892e1f
46kuri_ <ghost.46kuri@gmail.com>
2022-01-10 13:00:38 +0900

    Add files via upload

--- src/main/java/com/github/sirokuri_/JunkTeamPVP/listener/JunkTeamPVPStartTimer.java
+++ src/main/java/com/github/sirokuri_/JunkTeamPVP/listener/JunkTeamPVPStartTimer.java
@@ -25,32 +25,27 @@ public class JunkTeamPVPStartTimer implements Listener {
     public void startTimer() {
         plugin.task = new BukkitRunnable() {
             int countdownStarter = 20;
-            BossBar bossBar = Bukkit.createBossBar(ChatColor.DARK_PURPLE + "チーム対抗PVP : マッチ終了まで" + countdownStarter + " 秒", BarColor.PURPLE, BarStyle.SOLID);
+            BossBar bossBar = Bukkit.createBossBar(ChatColor.DARK_PURPLE + "試合終了まで" + countdownStarter + " 秒", BarColor.PURPLE, BarStyle.SOLID);
             int matchPlayers = 1;
             @Override
             public void run() {
                 if (plugin.redTeam.size() + plugin.blueTeam.size() == matchPlayers) {
                     plugin.getServer().broadcastMessage(ChatColor.DARK_PURPLE + "エントリーが" + matchPlayers + "以上の為ゲームを開始します");
                     ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-                    Runnable runnable = new Runnable() {
-                        @Override
-                        public void run() {
-                            countdownStarter--;
-                            if (countdownStarter > 0) {
-                                for (Player players : Bukkit.getServer().getOnlinePlayers()) {
-                                    bossBar.addPlayer(players);
-                                }
-                                bossBar.setTitle(ChatColor.DARK_PURPLE + "チーム対抗PVP : マッチ終了まで" + countdownStarter  + " 秒");
-                            } else if (countdownStarter == 0) {
-                                plugin.getServer().broadcastMessage(ChatColor.DARK_PURPLE + "試合時間が0になったので試合を終了します!");
-                                for (Player players : Bukkit.getServer().getOnlinePlayers()) {
-                                    bossBar.removePlayer(players);
-                                }
-                                scheduler.shutdown();
-                                plugin.redTeam.clear();
-                                plugin.blueTeam.clear();
-                                plugin.onlinePlayers.clear();
+                    Runnable runnable = () -> {
+                        countdownStarter--;
+                        if (countdownStarter > 0) {
+                            for (Player players : Bukkit.getServer().getOnlinePlayers()) {
+                                bossBar.addPlayer(players);
                             }
+                            bossBar.setTitle(ChatColor.DARK_PURPLE + "試合終了まで" + countdownStarter  + " 秒");
+                        } else if (countdownStarter == 0) {
+                            plugin.getServer().broadcastMessage(ChatColor.DARK_PURPLE + "試合時間が0になったので試合を終了します!");
+                            scheduler.shutdown();
+                            bossBar.removeAll();
+                            plugin.onlinePlayers.clear();
+                            plugin.redTeam.clear();
+                            plugin.blueTeam.clear();
                         }
                     };
                     scheduler.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
