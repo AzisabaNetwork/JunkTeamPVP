291bfb160f98439ed83689f0bf68333af9017d6c
46kuri_ <ghost.46kuri@gmail.com>
2022-01-10 15:40:19 +0900

    二回目からボスバー表示されないバグを修正

--- src/main/java/com/github/sirokuri_/JunkTeamPVP/listener/JunkTeamPVPStartTimer.java
+++ src/main/java/com/github/sirokuri_/JunkTeamPVP/listener/JunkTeamPVPStartTimer.java
@@ -12,6 +12,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class JunkTeamPVPStartTimer implements Listener {
 
@@ -19,39 +20,39 @@ public class JunkTeamPVPStartTimer implements Listener {
 
     public JunkTeamPVPStartTimer(JunkTeamPVP junkTeamPVP) {
         this.plugin = junkTeamPVP;
-        startTimer();
-    }
-
-    public void startTimer() {
         plugin.task = new BukkitRunnable() {
-            int countdownStarter = 20;
-            BossBar bossBar = Bukkit.createBossBar(ChatColor.DARK_PURPLE + "チーム対抗PVP : マッチ終了まで" + countdownStarter + " 秒", BarColor.PURPLE, BarStyle.SOLID);
-            int matchPlayers = 1;
             @Override
             public void run() {
-                if (plugin.redTeam.size() + plugin.blueTeam.size() == matchPlayers) {
-                    plugin.getServer().broadcastMessage(ChatColor.DARK_PURPLE + "エントリーが" + matchPlayers + "以上の為ゲームを開始します");
-                    ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-                    Runnable runnable = () -> {
-                        countdownStarter--;
-                        if (countdownStarter > 0) {
-                            for (Player players : Bukkit.getServer().getOnlinePlayers()) {
-                                bossBar.addPlayer(players);
-                            }
-                            bossBar.setTitle(ChatColor.DARK_PURPLE + "チーム対抗PVP : マッチ終了まで" + countdownStarter  + " 秒");
-                        } else if (countdownStarter == 0) {
-                            plugin.getServer().broadcastMessage(ChatColor.DARK_PURPLE + "試合時間が0になったので試合を終了します!");
-                            scheduler.shutdown();
-                            bossBar.removeAll();
-                            plugin.onlinePlayers.clear();
-                            plugin.redTeam.clear();
-                            plugin.blueTeam.clear();
-                        }
-                    };
-                    scheduler.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
-                }
+                startTimer();
             }
         };
         plugin.task.runTaskTimer(plugin, 0, 1000);
+    }
+
+    public void startTimer() {
+        AtomicInteger countdownStarter = new AtomicInteger(20);
+        BossBar bossBar = Bukkit.createBossBar(ChatColor.DARK_PURPLE + "チーム対抗PVP : マッチ終了まで" + countdownStarter + " 秒", BarColor.PURPLE, BarStyle.SOLID);
+        int matchPlayers = 1;
+        if (plugin.redTeam.size() + plugin.blueTeam.size() == matchPlayers) {
+            plugin.getServer().broadcastMessage(ChatColor.DARK_PURPLE + "エントリーが" + matchPlayers + "以上の為ゲームを開始します");
+            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+            Runnable runnable = () -> {
+                countdownStarter.getAndDecrement();
+                if (countdownStarter.get() > 0) {
+                    for (Player players : Bukkit.getServer().getOnlinePlayers()) {
+                        bossBar.addPlayer(players);
+                    }
+                    bossBar.setTitle(ChatColor.DARK_PURPLE + "チーム対抗PVP : マッチ終了まで" + countdownStarter  + " 秒");
+                } else if (countdownStarter.get() == 0) {
+                    plugin.getServer().broadcastMessage(ChatColor.DARK_PURPLE + "試合時間が0になったので試合を終了します!");
+                    scheduler.shutdown();
+                    bossBar.removeAll();
+                    plugin.onlinePlayers.clear();
+                    plugin.redTeam.clear();
+                    plugin.blueTeam.clear();
+                }
+            };
+            scheduler.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
+        }
     }
 }
