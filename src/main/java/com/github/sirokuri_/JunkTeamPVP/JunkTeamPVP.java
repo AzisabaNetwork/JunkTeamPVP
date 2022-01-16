a2e12818b83b49843cc6c12b8d7bc5aae25f0505
46kuri_ <ghost.46kuri@gmail.com>
2022-01-16 17:33:03 +0900

    存在しないコマンドがあったので修正

--- src/main/java/com/github/sirokuri_/JunkTeamPVP/JunkTeamPVP.java
+++ src/main/java/com/github/sirokuri_/JunkTeamPVP/JunkTeamPVP.java
@@ -2,7 +2,6 @@ package com.github.sirokuri_.JunkTeamPVP;
 
 import com.github.sirokuri_.JunkTeamPVP.Command.JunkTeamPVPCommand;
 import com.github.sirokuri_.JunkTeamPVP.listener.*;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -16,7 +15,6 @@ public class JunkTeamPVP extends JavaPlugin {
     public List<Player> redTeam = new ArrayList<Player>();
     public List<Player> blueTeam = new ArrayList<Player>();
     public BukkitRunnable task = null;
-    public FileConfiguration config;
 
     @Override
     public void onEnable() {
@@ -25,8 +23,7 @@ public class JunkTeamPVP extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new JunkTeamPVPGuard(this), this);
         getServer().getPluginManager().registerEvents(new JunkTeamPVPStartTimer(this), this);
         getServer().getPluginManager().registerEvents(new JunkTeamPVPBlockBreak(this), this);
-        getCommand("sb").setExecutor(new JunkTeamPVPCommand(this));
-        saveDefaultConfig();
+        getCommand("jtPVP").setExecutor(new JunkTeamPVPCommand(this));
     }
 
     @Override
